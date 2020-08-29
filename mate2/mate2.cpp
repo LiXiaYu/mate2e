a8298b2feec8fcb2c884babca59bc854ea39d952
Li XiaYu <liqixbd@hotmail.com>
2020-08-29 17:56:26 +0800

    token：#，"",///**/

--- mate2/mate2.cpp
+++ mate2/mate2.cpp
@@ -13,13 +13,40 @@ int main()
     std::vector<mate::token> ts;
 
     std::vector<std::unique_ptr<mate::tokenizer>> cts;
+    
+    cts.push_back(std::unique_ptr<mate::tokenizer>(dynamic_cast<mate::tokenizer*>(new mate::regex_tokenizer("#_lines", R"eee(#([^\n]+\\\n)+[^\n]+\n)eee"))));
+    cts.push_back(std::unique_ptr<mate::tokenizer>(dynamic_cast<mate::tokenizer*>(new mate::regex_tokenizer("#_line", R"eee(#[^\n]+[^\\]\n)eee"))));
+
+    cts.push_back(std::unique_ptr<mate::tokenizer>(dynamic_cast<mate::tokenizer*>(new mate::regex_tokenizer("commit_line", R"(//[\S\s]+\n)"))));
+    cts.push_back(std::unique_ptr<mate::tokenizer>(dynamic_cast<mate::tokenizer*>(new mate::regex_tokenizer("commit_lines", R"(/\*[\S\s]+\*/)"))));
+
+    cts.push_back(std::unique_ptr<mate::tokenizer>(dynamic_cast<mate::tokenizer*>(new mate::regex_tokenizer("string", R"("[\S\s]+")"))));
+    cts.push_back(std::unique_ptr<mate::tokenizer>(dynamic_cast<mate::tokenizer*>(new mate::regex_tokenizer("rawstring", "R\"[\\S\\s]+\""))));
 
     cts.push_back(std::unique_ptr<mate::tokenizer>(dynamic_cast<mate::tokenizer*>(new mate::regex_tokenizer("{}", R"(\{[\S\s]+\})"))));
     cts.push_back(std::unique_ptr<mate::tokenizer>(dynamic_cast<mate::tokenizer*>(new mate::char_tokenizer("MateTag", "mate"))));
     cts.push_back(std::unique_ptr<mate::tokenizer>(dynamic_cast<mate::tokenizer*>(new mate::char_tokenizer("Block", " "))));
     //cts.push_back(std::unique_ptr<mate::tokenizer>(dynamic_cast<mate::tokenizer*>(new mate::regex_tokenizer("*+", R"({\S+\})"))));
 
-    std::string text = R"( mate ass{ fuckmate}; )";
+    std::string text = R"eeeee( #include "mate2.h"
+
+#include"parser/regex_tokenizer.h"
+#include "parser/char_tokenizer.h"
+#define A ++++\
+++b
+
+#ifndef bool
+#define ture 1
+#define false 0
+#endif
+
+#include "parser/token.h"
+mate ass{ fuckmate};
+//一个string
+auto s="(heheheh)";
+/*一个R"()"的raw string*/
+auto ssr=R"(heheheh)";
+ )eeeee";
 
     auto itor_none_begin = text.begin();
     auto itor_none_end = itor_none_begin;
